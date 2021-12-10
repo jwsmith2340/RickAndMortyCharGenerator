6e7228dc53474f2c0190bcb513e9871bb0ba754a
James Smith <jwsmith2340@yahoo.com>
2021-12-10 17:35:35 -0500

    Linked to API, found properties I want to access

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('working')
+const $test = $.ajax('https://rickandmortyapi.com/api/character/?name=rick')
+console.log($test)
+
+
+$test.then(function(evt){
+    console.log(evt)
+    console.log(evt.results[0].name)
+    console.log(evt.results[0].species)
+    console.log(evt.results[0].status)
+})
