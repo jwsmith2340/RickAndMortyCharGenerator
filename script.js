81f80c8a497f74d1e76d4fa86a55fedbe18c62bc
James Smith <jwsmith2340@yahoo.com>
2021-12-10 18:57:07 -0500

    text input and submit button areup

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const $test = $.ajax('https://rickandmortyapi.com/api/character/?name=rick')
 const $main = $('main')
-console.log($test)
+const $input = $('input[type="text"]')
+const $form = $('form')
+
+$form.on('submit', function(event){
+    event.preventDefault();
+    if (!$input.val()) return;
+    const userValue = $input.val();
+    console.log(userValue)
+})
 
 
-$test.then(render)
 
-function render(evt){
-    $main.html(`
-        <p>Name: ${evt.results[0].name}</p>
-        <p>Species: ${evt.results[0].species}</p>
-        <p>Status: ${evt.results[0].status}</p>
-        <div><img src="${evt.results[0].image}"></div>
-    `)
-}
+
+
+
+
+
+
+
+//const $test = $.ajax('https://rickandmortyapi.com/api/character/?name=rick')
+//$test.then(render)
+
+// function render(evt){
+//     $main.html(`
+//         <p>Name: ${evt.results[0].name}</p>
+//         <p>Species: ${evt.results[0].species}</p>
+//         <p>Status: ${evt.results[0].status}</p>
+//         <div><img src="${evt.results[0].image}"></div>
+//     `)
+// }
