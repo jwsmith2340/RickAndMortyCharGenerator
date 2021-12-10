c4077aedd7806073447e45469d8f8beb4c8dc33d
James Smith <jwsmith2340@yahoo.com>
2021-12-10 17:41:21 -0500

    Successfully rendering test values to HTML

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const $test = $.ajax('https://rickandmortyapi.com/api/character/?name=rick')
+const $main = $('main')
 console.log($test)
 
 
-$test.then(function(evt){
-    console.log(evt)
-    console.log(evt.results[0].name)
-    console.log(evt.results[0].species)
-    console.log(evt.results[0].status)
-})
+$test.then(render)
+
+function render(evt){
+    $main.html(`
+        <p>Name: ${evt.results[0].name}</p>
+        <p>Species: ${evt.results[0].species}</p>
+        <p>Status: ${evt.results[0].status}</p>
+        <div><img src="${evt.results[0].image}"></div>
+    `)
+}
