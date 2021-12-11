1e875a83d22df05759461d01bad9a24ec8ac5e0e
James Smith <jwsmith2340@yahoo.com>
2021-12-10 19:10:45 -0500

    Main page is returning index 0 result

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
+//Global variable declarations
 const $main = $('main')
 const $input = $('input[type="text"]')
 const $form = $('form')
+const URL = 'https://rickandmortyapi.com/api/character/?name='
 
-$form.on('submit', function(event){
-    event.preventDefault();
-    if (!$input.val()) return;
-    const userValue = $input.val();
-    console.log(userValue)
-})
-
-
-
-
-
-
-
-
-
+//Event initiation
+$form.on('submit', handleSubmit)
 
 //const $test = $.ajax('https://rickandmortyapi.com/api/character/?name=rick')
 //$test.then(render)
 
-// function render(evt){
-//     $main.html(`
-//         <p>Name: ${evt.results[0].name}</p>
-//         <p>Species: ${evt.results[0].species}</p>
-//         <p>Status: ${evt.results[0].status}</p>
-//         <div><img src="${evt.results[0].image}"></div>
-//     `)
-// }
+function render(evt){
+    $main.html(`
+    <p>Name: ${evt.results[0].name}</p>
+    <p>Species: ${evt.results[0].species}</p>
+    <p>Status: ${evt.results[0].status}</p>
+    <div><img src="${evt.results[0].image}"></div>
+    `)
+}
+
+function handleSubmit(event){
+    event.preventDefault(); //prevent page reload
+    if (!$input.val()) return; //user must input text
+    const userValue = $input.val(); //assigning user input so we can clear text box
+    //console.log(userValue)
+    $input.val(''); //clears the input field
+
+    $.ajax(URL + userValue).then(function(param){
+        render(param);
+    },
+    function(error){
+        console.log('Resultant error: ' + error);
+    })
+}
