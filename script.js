0cec8631ecc9c423632e6cab0c5ab81332115fec
James Smith <jwsmith2340@yahoo.com>
2021-12-12 12:58:58 -0500

    commented out working code to begin work on a second feature, randomizer from over 800 characters

--- script.js
+++ script.js
@@ -1,47 +1,93 @@
-//Global variable declarations
-const $main = $('main')
-const $input = $('input[type="text"]')
-const $form = $('form')
-const URL = 'https://rickandmortyapi.com/api/character/?name='
+// //Global variable declarations
+// const $main = $('main')
+// const $input = $('input[type="text"]')
+// const $form = $('form')
+// const URL = 'https://rickandmortyapi.com/api/character/?name='
 
-//Event initiation
-$form.on('submit', handleSubmit)
+// //Event initiation
+// $form.on('submit', handleSubmit)
 
-//Function Block
+// //Function Block
 
-//Render retrieved values to HTML
-function render(evt){ 
-    let randomIndex = arrayRandomizer(evt);
+// //Render retrieved values to HTML
+// function render(evt){ 
+//     let randomIndex = arrayRandomizer(evt);
         
-    $main.html(`
-        <p>Name: ${evt.results[randomIndex].name}</p>
-        <p>Species: ${evt.results[randomIndex].species}</p>
-        <p>Status: ${evt.results[randomIndex].status}</p>
-        <div><img src="${evt.results[randomIndex].image}"></div>
-    `)
-}
+//     $main.html(`
+//         <p>Name: ${evt.results[randomIndex].name}</p>
+//         <p>Species: ${evt.results[randomIndex].species}</p>
+//         <p>Status: ${evt.results[randomIndex].status}</p>
+//         <div><img src="${evt.results[randomIndex].image}"></div>
+//     `)
+// }
 
-//Handle submit button, prevent page reload, require user input text, and assign
-//The user's val to a local variable to be used to concatonate the URL for AJAX request
-//Input field is then cleared, ajax retrieval initiates render(), error contingency in place
-function handleSubmit(event){
+// //Handle submit button, prevent page reload, require user input text, and assign
+// //The user's val to a local variable to be used to concatonate the URL for AJAX request
+// //Input field is then cleared, ajax retrieval initiates render(), error contingency in place
+// function handleSubmit(event){
+//     event.preventDefault();
+//     if (!$input.val()) return; 
+//     const userValue = $input.val(); 
+//     $input.val(''); 
+
+//     $.ajax(URL + userValue).then(function(param){
+//         render(param);
+//     },
+//     function(error){
+//         //console.log('Resultant error: ' + error);
+//     })
+// }
+
+// //Arrays for characters are different lengths, this code block returns a random number within
+// //the range for each character selected so the full catalog can be accessed at random. 
+// function arrayRandomizer(eo){
+//     let randomLength = eo.results.length 
+//     let randomIndex = Math.floor(Math.random() * randomLength); 
+//     return randomIndex;
+// }
+
+
+//Overcoming Pagination Limiting My Searches (Hopefully)
+const pageURL = 'https://rickandmortyapi.com/api/character/?page=';
+//console.log($.ajax('https://rickandmortyapi.com/api/character'));
+//let allNames = [];
+let allCharactersArray = []
+let nameArray = []
+
+allCharactersArray = ($.ajax(pageURL).then(function(evt){
+    const pageLength = evt.info.pages
+    nameArray;
+    for (let i = 1; i <= pageLength; i++){
+        $.ajax(pageURL + i).then(function(evt2){
+            for (let j = 0; j < pageLength; j++){
+                nameArray.push(evt2.results[j].name)                
+            }
+        })
+    }
+}))
+
+$('form').on('submit', function(event){
     event.preventDefault();
-    if (!$input.val()) return; 
-    const userValue = $input.val(); 
-    $input.val(''); 
+    console.log(nameArray.length)
+    let randomNum = Math.floor(Math.random() * nameArray.length)
+    console.log(randomNum);
+    console.log(nameArray[randomNum])
+    //console.log(nameArray);
+    //nameArray.forEach(nm => console.log(nm))  LOGS ALL 800 SOME ODD CHARACTERS
+    
+    // let soughtChar = [];         DOES NOT WORK
+    // soughtChar = nameArray.map(function(id){
+    //     if (id == 'rick'){
+    //         console.log(id);  
+    //     }
+    // })
+    // console.log(soughtChar)
 
-    $.ajax(URL + userValue).then(function(param){
-        render(param);
-    },
-    function(error){
-        console.log('Resultant error: ' + error);
-    })
+    
+    
 }
+)
 
-//Arrays for characters are different lengths, this code block returns a random number within
-//the range for each character selected so the full catalog can be accessed at random. 
-function arrayRandomizer(eo){
-    let randomLength = eo.results.length 
-    let randomIndex = Math.floor(Math.random() * randomLength); 
-    return randomIndex;
-}
+$('#pickleRick').on('click', function(listener){
+    alert('clicked')
+})
