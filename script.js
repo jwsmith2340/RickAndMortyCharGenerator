d5bd7f06fa40e05a7b197ed99f436e19c035ee86
James Smith <jwsmith2340@yahoo.com>
2021-12-13 12:31:21 -0500

    cleaned up code, organized it

--- script.js
+++ script.js
@@ -3,72 +3,6 @@ const $main = $('main')
 const $input = $('input[type="text"]')
 const $form = $('form')
 const URL = 'https://rickandmortyapi.com/api/character/?name='
-
-//Event initiation
-$form.on('submit', handleSubmit)
-
-//Function Block
-
-//Render retrieved values to HTML
-function render(evt){ 
-    let randomIndex = arrayRandomizer(evt);
-        
-    $main.html(`
-        <p>Name: ${evt.results[randomIndex].name}</p>
-        <p>Species: ${evt.results[randomIndex].species}</p>
-        <p>Status: ${evt.results[randomIndex].status}</p>
-        <div><img src="${evt.results[randomIndex].image}"></div>
-    `)
-}
-
-//Handle submit button, prevent page reload, require user input text, and assign
-//The user's val to a local variable to be used to concatonate the URL for AJAX request
-//Input field is then cleared, ajax retrieval initiates render(), error contingency in place
-function handleSubmit(event){
-    event.preventDefault();
-    if (!$input.val()) return; 
-    const userValue = $input.val(); 
-    $input.val(''); 
-
-    $.ajax(URL + userValue).then(function(param){
-        render(param);
-    },
-    function(error){
-        //console.log('Resultant error: ' + error);
-    })
-}
-
-//Arrays for characters are different lengths, this code block returns a random number within
-//the range for each character selected so the full catalog can be accessed at random. 
-function arrayRandomizer(eo){
-    let randomLength = eo.results.length 
-    let randomIndex = Math.floor(Math.random() * randomLength); 
-    return randomIndex;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//************************************************************************************* */
-//Overcoming Pagination Limiting My Searches (Hopefully)
 const pageURL = 'https://rickandmortyapi.com/api/character/?page=';
 let allCharactersArray = []
 let nameArray = []
@@ -85,62 +19,75 @@ allCharactersArray = ($.ajax(pageURL).then(function(evt){
     }
 }))
 
-// $('form').on('submit', function(event){
-//     event.preventDefault();
-//     console.log(nameArray.length)
-//     let randomNum = Math.floor(Math.random() * nameArray.length)
-    //console.log(randomNum);   random num between 1 and 826
-    //console.log(nameArray[randomNum])   Logs random array between 1 and 826
-    //console.log(nameArray);
-    //nameArray.forEach(nm => console.log(nm))  LOGS ALL 800 SOME ODD CHARACTERS
-    
-    // let soughtChar = [];         DOES NOT WORK
-    // soughtChar = nameArray.map(function(id){
-    //     if (id == 'rick'){
-    //         console.log(id);  
-    //     }
-    // })
-    // console.log(soughtChar)
+//Event initiation
+$form.on('submit', handleSubmit)   //Form submit event
+$('#pickleRick').on('click', handleRandomEvent) //Pickle Rick event
 
-    
-    
-// }
-// )
+//Function Block
 
-$('#pickleRick').on('click', function(listener){
-    
-    listener.preventDefault();
-    let randomNum = Math.floor(Math.random() * nameArray.length)
-    //console.log(nameArray[randomNum])
-    let testSpaces = nameArray[randomNum]
-    let testFirstFix = testSpaces.split(' ').join('+')
-    //console.log(testFirstFix)
-    let testSecondFix = testFirstFix.split('-').join('+')
-    //console.log(testSecondFix)
-
-    let testURL = URL + testSecondFix
-    //console.log(testURL)
-
-    $.ajax(testURL).then(function(par){
-        renderRandom(par)
-    })
-
-
-
-
-//    $main.html(`
-//         <p>Name: ${evt.results[randomIndex].name}</p>
-//         <p>Species: ${evt.results[randomIndex].species}</p>
-//         <p>Status: ${evt.results[randomIndex].status}</p>
-//         <div><img src="${evt.results[randomIndex].image}"></div>
-//     `)
-})
-
-function renderRandom(randEvt){
+//Input Submit - Render retrieved values to HTML
+function render(evt){ 
+    let randomIndex = arrayRandomizer(evt);
+        
+    $main.html(`
+        <p>Name: ${evt.results[randomIndex].name}</p>
+        <p>Species: ${evt.results[randomIndex].species}</p>
+        <p>Status: ${evt.results[randomIndex].status}</p>
+        <div><img src="${evt.results[randomIndex].image}"></div>
+    `)
+}
+//Pickle Rick - Render retrieved values to HTML
+function renderRandom(randEvt){  
     $main.html(`
         <p>Name: ${randEvt.results[0].name}</p>
         <p>Species: ${randEvt.results[0].species}</p>
         <p>Status: ${randEvt.results[0].status}</p>
         <div><img src="${randEvt.results[0].image}"></div>
     `)
+}
+
+//Handle submit button, prevent page reload, require user input text, and assign
+//The user's val to a local variable to be used to concatonate the URL for AJAX request
+//Input field is then cleared, ajax retrieval initiates render(), error contingency in place
+function handleSubmit(event){
+    event.preventDefault();
+    if (!$input.val()) return; 
+    const userValue = $input.val(); 
+    $input.val(''); 
+
+    $.ajax(URL + userValue).then(function(param){
+        render(param);
+    },
+    function(error){
+        console.log('Resultant error: ' + error);
+    })
+}
+//Handle Pickle Rick event, prevent page reload, generate random number based on 
+//the length of all characters in the API, remove spaces and inser + chars for the
+//URL rules, remove - and insert + chars to obey URL rules, add URL and formatted 
+//name to a URL variable to be searched with AJAX, search, for that character via
+//promise, then call renderRandom function
+function handleRandomEvent(listener){
+    listener.preventDefault();
+
+    let randomNum = Math.floor(Math.random() * nameArray.length)
+    let testSpaces = nameArray[randomNum]
+    let testFirstFix = testSpaces.split(' ').join('+')
+    let testSecondFix = testFirstFix.split('-').join('+')
+    let testURL = URL + testSecondFix
+    
+    $.ajax(testURL).then(function(par){
+        renderRandom(par)
+    }, 
+    function(error){
+        console.log('Resultanat error: ' + error)
+    })
+}
+
+//Arrays for characters are different lengths, this code block returns a random number within
+//the range for each character selected so the full catalog can be accessed at random. 
+function arrayRandomizer(eo){
+    let randomLength = eo.results.length 
+    let randomIndex = Math.floor(Math.random() * randomLength); 
+    return randomIndex;
 }
