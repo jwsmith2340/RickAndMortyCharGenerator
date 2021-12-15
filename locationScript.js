0fbd2519f442fab2a6edaef5f3c90b934465c2f4
James Smith <jwsmith2340@yahoo.com>
2021-12-15 08:51:14 -0500

    working properly, randomizer and input both working properly, all that is left is some tidying up code

--- locationScript.js
+++ locationScript.js
@@ -1,15 +1,21 @@
-const URL = 'https://rickandmortyapi.com/api/location/'
+//GLOBAL 
+const URL = 'https://rickandmortyapi.com/api/location'
 const URLname = 'https://rickandmortyapi.com/api/location/?name='
+const URLpage = 'https://rickandmortyapi.com/api/location/?page='
 const $main = $('main')
 const $input = $('input')
 const $form = $('form')
 let page;
 let userInput;
 
+//Event initiation
 $form.on('submit', handleSubmit)
+$('#pickleRick').on('click', handlePickleRick)
 
+//Form input - handles user submissions
 function handleSubmit(evt){
     evt.preventDefault();
+    $('#main_small').html('')
     $('#main').html('');
     userInput = $input.val();
     if (userInput === '') return;
@@ -23,39 +29,65 @@ function handleSubmit(evt){
     })
 }
 
-function randomizePlanet(data){
-    pageRandom(data.info.pages)
-    $.ajax(URLname + userInput + '&page=' + page).then(function(final){
-        //console.log(final)
-        renderPlanet(final)
+//Pickle Rick - Handles random event
+function handlePickleRick(evt){
+    evt.preventDefault();
+    $main.html('')
+    $('#main').html('')
+    $('#main_small').html('')
+
+    $.ajax(URL).then(function(par){
+        console.log(par)
+        pickleRandomizePlanet(par)
     })
 }
 
+//Calls page randomizer function, ajax request, initiates renderPlanet function
+function randomizePlanet(data){
+    pageRandom(data.info.pages)
+    $.ajax(URLname + userInput + '&page=' + page).then(function(final){
+    renderPlanet(final)
+    })
+}
+
+function pickleRandomizePlanet(data){
+    pageRandom(data.info.pages)
+    $.ajax(URLpage + page).then(function(info){
+        renderPlanet(info)
+    })
+}
+
+//function to randomize page results from pagination so all locations can be accessed
 function pageRandom(num){
     page = Math.floor(Math.random() * (num + 1 - 1) + 1)
 }
 
+//render planet with random array
 function renderPlanet(event){
-    console.log(event)
     let randomArray = Math.floor(Math.random() * event.results.length)
-        
-        $main.html(`
+    $main.html(`
         <p>Name: ${event.results[randomArray].name }`)
-        //console.log(event.results[randomArray].residents)
         renderChars(event.results[randomArray].residents)
-        // $('#main').append(`
-        // <img src="${event.results.image}"`)
 }
 
+//Render character images
 function renderChars(evt){
     if (evt.length === 0){
-        $('main').append('<p><strong>There are no permanent residents on this planet</strong>')
+        $('main').append('<p><strong>There are no permanent residents of this location</strong>')
     }
-    for (let i = 0; i < evt.length; i++){
-        //if (i === 28){ return;}
+
+    if (evt.length < 5){
+        for (let i = 0; i < evt.length; i++){
+            $.ajax(evt[i]).then(function(name){
+                console.log(name)
+                $('#main_small').append(`<div class="location_small"><img class="location_small_images" src="${name.image}"</div>`)   
+            })  
+        }
+    }else if (evt.length >= 5) {
+        for (let i = 0; i < evt.length; i++){
         $.ajax(evt[i]).then(function(name){
             console.log(name)
             $('#main').append(`<div class="episode_div"><img class="episode_images" src="${name.image}"</div>`)   
         })  
-    }
+    }}
 }
